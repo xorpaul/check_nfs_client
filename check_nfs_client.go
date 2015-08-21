17d7353b20511b6cfd7fbf01e90be3c7ca9eb599
Andreas Paul <andreas.paul@1und1.de>
2015-08-21 16:17:37 +0200

    use github.com/xorpaul/go-nagios

--- check_nfs_client.go
+++ check_nfs_client.go
@@ -3,14 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/kballard/go-shellquote"
+	"github.com/xorpaul/go-nagios"
 	"os"
-	"os/exec"
 	"regexp"
-	"strconv"
 	"strings"
-	"syscall"
-	"time"
 )
 
 var (
@@ -18,111 +14,27 @@ var (
 	buildtime string
 )
 
-type NagiosResult struct {
-	exitCode  int
-	text      string
-	perfdata  string
-	multiline []string
-}
-
-type ExecResult struct {
-	returnCode int
-	output     string
-}
-
-// Debugf is a helper function for debug logging if mainCfgSection["debug"] is set
-func Debugf(s string) {
-	if debug != false {
-		fmt.Println("DEBUG " + fmt.Sprint(s))
-	}
-}
-
-// nagiosExit uses the NagiosResult struct to output Nagios plugin compatible output and exit codes
-func nagiosExit(nr NagiosResult) {
-	text := nr.text
-	exitCode := nr.exitCode
-	switch {
-	case nr.exitCode == 0:
-		text = "OK: " + nr.text
-		exitCode = nr.exitCode
-	case nr.exitCode == 1:
-		text = "WARNING: " + nr.text
-		exitCode = nr.exitCode
-	case nr.exitCode == 2:
-		text = "CRITICAL: " + nr.text
-		exitCode = nr.exitCode
-	case nr.exitCode == 3:
-		text = "UNKNOWN: " + nr.text
-		exitCode = nr.exitCode
-	default:
-		text = "UNKNOWN: Exit code '" + string(nr.exitCode) + "'undefined :" + nr.text
-		exitCode = 3
-	}
-
-	if len(nr.multiline) > 0 {
-		multiline := ""
-		for _, l := range nr.multiline {
-			multiline = multiline + l + "\n"
-		}
-		fmt.Printf("%s|%s\n%s\n", text, nr.perfdata, multiline)
-	} else {
-		fmt.Printf("%s|%s\n", text, nr.perfdata)
-	}
-	os.Exit(exitCode)
-}
-
-func executeCommand(command string, timeout int, allowFail bool) ExecResult {
-	Debugf("Executing " + command)
-	parts := strings.SplitN(command, " ", 2)
-	cmd := parts[0]
-	cmdArgs := []string{}
-	if len(parts) > 1 {
-		args, err := shellquote.Split(parts[1])
-		if err != nil {
-			Debugf("executeCommand(): err: " + fmt.Sprint(err))
-			os.Exit(1)
-		} else {
-			cmdArgs = args
-		}
-	}
-
-	before := time.Now()
-	out, err := exec.Command(cmd, cmdArgs...).CombinedOutput()
-	duration := time.Since(before).Seconds()
-	er := ExecResult{0, string(out)}
-	if msg, ok := err.(*exec.ExitError); ok { // there is error code
-		er.returnCode = msg.Sys().(syscall.WaitStatus).ExitStatus()
-	}
-	Debugf("Executing " + command + " took " + strconv.FormatFloat(duration, 'f', 5, 64) + "s")
-	if err != nil && !allowFail {
-		fmt.Println("executeCommand(): command failed: "+command, err)
-		fmt.Println("executeCommand(): Output: " + string(out))
-		os.Exit(1)
-	}
-	return er
-}
-
-func parseNfsstatOutput(s string) NagiosResult {
+func parseNfsstatOutput(s string) nagios.NagiosResult {
 	perfdata := ""
 	var multiline []string
 	reMetric := regexp.MustCompile("\\s+([a-z]+):\\s+(\\d+)")
 	data := strings.Split(s, "\n")
 	if len(data) == 1 {
-		return NagiosResult{text: "nfsstat output was empty", exitCode: 1, perfdata: ""}
+		return nagios.NagiosResult{Text: "nfsstat output was empty", ExitCode: 1, Perfdata: ""}
 	}
 	for _, l := range data {
 		if len(l) == 0 {
 			continue
 		}
-		Debugf("line: " + l)
+		nagios.Debugf("line: " + l)
 		if m := reMetric.FindStringSubmatch(l); len(m) > 1 {
-			Debugf("metric: " + m[1] + "value: " + m[2])
+			nagios.Debugf("metric: " + m[1] + "value: " + m[2])
 			perfdata = perfdata + m[1] + "=" + m[2] + "c "
 			multiline = append(multiline, m[0])
 		}
 	} // close for loop over output
-	Debugf(perfdata)
-	return NagiosResult{text: "nfsstat output successfully parsed", exitCode: 0, perfdata: perfdata, multiline: multiline}
+	nagios.Debugf(perfdata)
+	return nagios.NagiosResult{Text: "nfsstat output successfully parsed", ExitCode: 0, Perfdata: perfdata, Multiline: multiline}
 }
 
 func main() {
@@ -133,17 +45,18 @@ func main() {
 	flag.Parse()
 
 	debug = *debugFlag
+	nagios.Debug = *debugFlag
 
 	if *versionFlag {
 		fmt.Println("check_nfs_client Version 0.1 Build time:", buildtime, "UTC")
 		os.Exit(0)
 	}
 
-	nr := NagiosResult{exitCode: 3, text: "uncatched case", perfdata: ""}
+	nr := nagios.NagiosResult{ExitCode: 3, Text: "uncatched case", Perfdata: ""}
 
-	er := executeCommand("nfsstat -c -l", 1, false)
-	nr = parseNfsstatOutput(er.output)
+	er := nagios.ExecuteCommand("nfsstat -c -l", 1, false)
+	nr = parseNfsstatOutput(er.Output)
 
-	nagiosExit(nr)
+	nagios.NagiosExit(nr)
 
 }
